77b78f0f3ffa3b7a587b56a98761d1787fbd6124
Keep Moving <keepmoving.motivateyourself@gmail.com>
2022-09-08 12:20:02 +0530

    Actuator support added for springfox

--- 4.Microservices/sample-ms/README.md
+++ 4.Microservices/sample-ms/README.md
@@ -8,7 +8,10 @@ A spring boot application with both swagger 3.0 api documentation enabled
 ```
 
 ## Paths
-- [All Swagger Resources(groups)](http://localhost:8080/sample-ms/swagger-resources)
-- [All API Docs](http://localhost:8080/sample-ms/v3/api-docs)
-- [Swagger UI endpoint](http://localhost:8080/sample-ms/swagger-ui/index.html)
+- [All Swagger Resources(groups)](http://localhost:9000/sample-ms/swagger-resources)
+- [All API Docs](http://localhost:9000/sample-ms/v3/api-docs)
+- [Swagger UI endpoint](http://localhost:9000/sample-ms/swagger-ui/index.html)
 
+## Actuator path
+- [Actuator Endpoints](http://localhost:9001/sample-ms/actuator)
+- [Actuator Specific Endpoint(Loggers)](http://localhost:9001/sample-ms/actuator/loggers)
---
+++ 4.Microservices/sample-ms/src/main/java/com/touchblankspot/ms/samplems/config/SwaggerActuatorConfig.java
@@ -0,0 +1,53 @@
+package com.touchblankspot.ms.samplems.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * This is a temp config that needs to be removed once Springfox would be supporting actuator
+ */
+@Configuration
+public class SwaggerActuatorConfig {
+
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                        .filter(mapping -> mapping.getPatternParser() == null).toList();
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    assert field != null;
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+    }
+}
